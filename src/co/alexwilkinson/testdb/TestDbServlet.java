9f5831b08cc342b3200f47d368a6e50e315ae311
anontyro <alexwilkinson@gmail.com>
2017-01-24 14:43:51 +0800

    Adding the the resources needed
    making sure the basic routing is working fine

--- src/co/alexwilkinson/testdb/TestDbServlet.java
+++ src/co/alexwilkinson/testdb/TestDbServlet.java
@@ -1,11 +1,14 @@
 package co.alexwilkinson.testdb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
 
 /**
  * Servlet implementation class TestDbServlet
@@ -18,8 +21,40 @@ public class TestDbServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//setup connection vars
+		String user = "root";
+		String pass = "root";
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false"; 
+		
+		String driver = "com.mysql.jdbc.Driver";
+		
+		//get database connection
+		try{
+			
+			PrintWriter out = response.getWriter();
+			
+			out.println("connection to database: " +jdbcUrl + "<br>");
+			
+			Class.forName(driver);
+			
+			Connection myCon = DriverManager.getConnection(jdbcUrl, user, pass);
+			
+			out.println("Connected");
+			
+			myCon.close();
+			
+		}catch(Exception ex){
+			ex.getStackTrace();
+		}
+		
 	}
 
+	
+	
+	
+	
+	
+	
+	
 }
