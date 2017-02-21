10bd3812690a91567b8303efdaeabaecd4ffba69
anontyro <alexwilkinson@gmail.com>
2017-02-21 14:00:20 +0800

    Styling and basic format
    Added the resources folder to hold the static files and serving them.
    the index.jsp will redirect to the list view currently

--- WebContent/WEB-INF/view/list-customers.jsp
+++ WebContent/WEB-INF/view/list-customers.jsp
@@ -4,8 +4,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+
+<link type="text/css" rel="stylesheet" href="${pageContext.request.contextPath}/resources/style/bootstrap.min.css" />
+<link type="text/css" rel="stylesheet" href="${pageContext.request.contextPath}/resources/style/style.css" />
+
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>Customer List</title>
 </head>
 <body>
 
--- src/co/alexwilkinson/customertracker/controller/CustomerController.java
+++ src/co/alexwilkinson/customertracker/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import co.alexwilkinson.customertracker.dao.CustomerDAO;
@@ -19,7 +20,7 @@ public class CustomerController {
 	private CustomerDAO customerDAO;
 	
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model){
 		
 		//get customers
